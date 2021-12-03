400efdafd40814c07ddf60928804b4ff1c19d40e
Clément <clement.ramoslage@gmail.com>
2021-12-03 10:32:32 +0100

    feat: add invalid email test unit

--- tests/test_summary_page.py
+++ tests/test_summary_page.py
@@ -10,3 +10,11 @@ def test_post_summary_success(client, clubs, competitions):
         assert str.encode(competition['name']) in res.data
         assert str.encode(competition['date']) in res.data
         assert str.encode(competition['numberOfPlaces']) in res.data
+
+def test_post_summary_invalid_email(client):
+    res = client.post('/showSummary',
+        data=dict(email='invalid@email.com'),
+        follow_redirects=True)
+
+    assert res.status_code == 404
+    assert 'Désolé, cet email n&#39;a pas été trouvé' in res.data.decode()
