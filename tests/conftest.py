89fb07f2ccbbd67ad5779b5dddd8fa627d4fefd6
Clément <clement.ramoslage@gmail.com>
2021-12-03 22:14:57 +0100

    feat: add reset_data fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,17 @@ import json
 import pytest
 from server import app as flask_app
 
-from utils import load_clubs, load_competitions
+from utils import load_clubs, load_competitions, reset_jsons
 
 @pytest.fixture
 def client():
     flask_app.config['TESTING'] = True
     with flask_app.test_client() as client:
         yield client
+
+@pytest.fixture
+def reset_data():
+    return reset_jsons()
 
 @pytest.fixture(scope='module')
 def clubs():
--- utils.py
+++ utils.py
@@ -29,3 +29,13 @@ def update_competition_from_slug(slug, payload):
         json.dump({
             "competitions": [*filtred_competitions, payload]
         }, file, indent=4)
+
+def reset_jsons():
+    clubs = {'clubs': [{'name': 'She Lifts', 'slug': 'she-lifts', 'email': 'kate@shelifts.co.uk', 'points': '12'}, {'name': 'Iron Temple', 'slug': 'iron-temple', 'email': 'admin@irontemple.com', 'points': '4'}, {'name': 'Simply Lift', 'slug': 'simply-lift', 'email': 'john@simplylift.co', 'points': '675'}]}
+    competitions = {'competitions': [{'name': 'Fall Classic', 'slug': 'fall-classic', 'date': '2020-10-22 13:30:00', 'numberOfPlaces': '8'}, {'name': 'Spring Festival', 'slug': 'spring-festival', 'date': '2020-03-27 10:00:00', 'numberOfPlaces': '13'}]}
+
+    with open('competitions.json', 'w') as file:
+        json.dump(competitions, file, indent=4)
+
+    with open('clubs.json', 'w') as file:
+        json.dump(clubs, file, indent=4)
