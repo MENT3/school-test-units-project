3b01fc60810b6f9988db4fdae48a0d6985acc645
Clément <clement.ramoslage@gmail.com>
2021-12-05 22:39:12 +0100

    perf: close json file after processing

--- utils.py
+++ utils.py
@@ -9,37 +9,47 @@ def find_by(key, value, items):
     return next(item, False)
 
 def load_clubs():
-    with open('clubs.json') as club:
-        return json.load(club)['clubs']
+    clubs_file = open('clubs.json')
+    clubs_values = json.load(clubs_file)['clubs']
+    clubs_file.close()
+
+    return clubs_values
 
 def load_competitions():
-    with open('competitions.json') as comps:
-        return json.load(comps)['competitions']
+    competitions_file = open('competitions.json')
+    competitions_values = json.load(competitions_file)['competitions']
+    competitions_file.close()
+
+    return competitions_values
 
 def update_club_from_slug(slug, payload):
     clubs = load_clubs()
     filtred_clubs = [club for club in clubs if club['slug'] != slug]
 
-    with open('clubs.json', 'w') as file:
-        json.dump({
-            "clubs": [*filtred_clubs, payload]
-        }, file, indent=4)
+    clubs_file = open('clubs.json', 'w')
+    json.dump({
+        "clubs": [*filtred_clubs, payload]
+    }, clubs_file, indent=4)
+    clubs_file.close()
 
 def update_competition_from_slug(slug, payload):
     competitions = load_competitions()
     filtred_competitions = [competition for competition in competitions if competition['slug'] != slug]
 
-    with open('competitions.json', 'w') as file:
-        json.dump({
-            "competitions": [*filtred_competitions, payload]
-        }, file, indent=4)
+    competitions_file = open('competitions.json', 'w')
+    json.dump({
+        "competitions": [*filtred_competitions, payload]
+    }, competitions_file, indent=4)
+    competitions_file.close()
 
 def reset_jsons():
     clubs = {'clubs': [{'name': 'She Lifts', 'slug': 'she-lifts', 'email': 'kate@shelifts.co.uk', 'points': '12'}, {'name': 'Iron Temple', 'slug': 'iron-temple', 'email': 'admin@irontemple.com', 'points': '4'}, {'name': 'Simply Lift', 'slug': 'simply-lift', 'email': 'john@simplylift.co', 'points': '675'}]}
     competitions = {'competitions': [{'name': 'Fall Classic', 'slug': 'fall-classic', 'date': '2020-10-22 13:30:00', 'numberOfPlaces': '8'}, {'name': 'Spring Festival', 'slug': 'spring-festival', 'date': '2020-03-27 10:00:00', 'numberOfPlaces': '13'}]}
 
-    with open('competitions.json', 'w') as file:
-        json.dump(competitions, file, indent=4)
+    competitions_file = open('competitions.json', 'w')
+    json.dump(competitions, competitions_file, indent=4)
+    competitions_file.close()
 
-    with open('clubs.json', 'w') as file:
-        json.dump(clubs, file, indent=4)
+    clubs_file = open('clubs.json', 'w')
+    json.dump(clubs, clubs_file, indent=4)
+    clubs_file.close()
