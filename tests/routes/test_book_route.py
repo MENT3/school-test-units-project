95191c5a742546af8bf519d95292a4dea87392e3
Clément <clement.ramoslage@gmail.com>
2021-12-08 18:53:55 +0100

    test: add another logged in test

--- tests/routes/test_book_route.py
+++ tests/routes/test_book_route.py
@@ -1,4 +1,6 @@
+import random
 import pytest
+from random import choice
 
 def test_get_book_success(client, competitions):
     competition = competitions[0]
@@ -22,6 +24,7 @@ def test_get_book_success(client, competitions):
   ('spring-festival', 'invalid'),
   ('invalid', 'invalid')
 ])
+
 def test_get_book_with_invalid_slug(client, competition_slug, club_slug):
     with client.session_transaction() as sess:
         sess['club_slug'] = club_slug
@@ -38,5 +41,21 @@ def test_post_when_not_logged_in(client, competitions):
     res = client \
             .get('/book/{competition_slug}/simply-lift' \
             .format(competition_slug=competition['slug']))
+
+    assert res.status_code == 403
+
+def test_post_when_logged_in_but_unauthorized(client, clubs, competitions):
+    competition = competitions[0]
+
+    random_club = choice(clubs)
+    another_random_club = choice([club for club in clubs \
+        if club['slug'] != random_club['slug']])
+
+    with client.session_transaction() as sess:
+        sess['club_slug'] = random_club['slug']
+
+    res = client.get('/book/{competition_slug}/{club_slug}' \
+                .format(competition_slug=competition['slug'],
+                        club_slug=another_random_club['slug']))
 
     assert res.status_code == 403
